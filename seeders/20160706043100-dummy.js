4fcd1100e341ba7f6c9655cd7eeefa184a195477
Cathy Zhu <cathyxz@gmail.com>
2016-07-08 18:14:04 +0800

    style cleanup

--- seeders/20160706043100-dummy.js
+++ seeders/20160706043100-dummy.js
@@ -4,9 +4,9 @@ module.exports = {
   up: function (queryInterface, Sequelize) {
       return queryInterface.bulkInsert('Courses', [{
         title: "Mathematical Foundations of Computing",
-      catalog_id: "CS 103",
-      description: "Mathematical foundations required for computer science, including propositional predicate logic, induction, sets, functions, and relations. Formal language theory, including regular expressions, grammars, finite automata, Turing machines, and NP-completeness. Mathematical rigor, proof techniques, and applications. Prerequisite: 106A or equivalent.",
-      semester: "AUT1617"
+        catalog_id: "CS 103",
+        description: "Mathematical foundations required for computer science, including propositional predicate logic, induction, sets, functions, and relations. Formal language theory, including regular expressions, grammars, finite automata, Turing machines, and NP-completeness. Mathematical rigor, proof techniques, and applications. Prerequisite: 106A or equivalent.",
+        semester: "AUT1617"
       }], {});
   },
 
