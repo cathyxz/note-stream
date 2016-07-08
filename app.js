3bd3c2bbf6324d3f884b3758669d5cebd06e969d
Cathy Zhu <cathyxz@gmail.com>
2016-07-08 18:12:25 +0800

    added debugging libraries

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var path = require('path');
 var debug = require('debug');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var courses  = require('./routes/courses');
 
 var Sequelize = require('sequelize');
 var models = require('./models');
@@ -17,10 +21,15 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/public')));
 
 // routes setup
 app.use('/', routes);
+app.use('/courses', courses);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
